1d67f584fdd1caabcb6bf65c6eda7e1daa71b9ef
Mahir Asef <mahirasef30@gmail.com>
2024-10-03 18:21:52 +0600

    automation

---
+++ script.js
@@ -0,0 +1,27 @@
+const { Builder, Browser, By, Key, until } = require('selenium-webdriver')
+const chrome = require("selenium-webdriver/chrome");
+async function testRun() {
+    const productName = "Nike react phantom run flyknit 2";
+    let driver = await new Builder().forBrowser("chrome").build();
+    await driver.manage().window().maximize();
+    await driver.get("https://demo.evershop.io/");
+    // await driver.findElement(By.className("search-icon")).click();
+    await driver.findElement(By.xpath("//a[@class='search-icon']")).click();
+    await driver.findElement(By.xpath("//input[@placeholder='Search']")).sendKeys("nike");
+    // await driver.findElement(By.xpath("//input[@placeholder='Search']")).sendKeys("nike",Key.ENTER);
+    await driver.findElement(By.xpath("//input[@placeholder='Search']")).sendKeys(Key.ENTER);
+    await driver.findElement(By.xpath(`//a[contains(@href,'182')]/span[contains(text(),'${productName}')]`)).click();
+    await driver.findElement(By.xpath("//a[@href='#' and contains(text(),'S')]")).click();
+    await driver.sleep(3000);
+    await driver.findElement(By.xpath("//a[@href='#' and contains(text(),'Black')]")).click();
+    await driver.sleep(3000);
+    await driver.findElement(By.xpath("//input[@name='qty']")).clear();
+    await driver.findElement(By.xpath("//input[@name='qty']")).sendKeys(2);
+    await driver.findElement(By.xpath("//button[contains(.,'ADD TO CART')]")).click();
+    await driver.sleep(4000);
+    await driver.findElement(By.xpath("//a[@class='add-cart-popup-button']")).click();
+    
+    await driver.sleep(5000);
+    await driver.quit();
+}
+testRun();
